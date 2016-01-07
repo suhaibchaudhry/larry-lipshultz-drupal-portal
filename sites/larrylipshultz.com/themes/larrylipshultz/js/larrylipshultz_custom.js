331b708ef710e12b7b61a791536ecdd3478075f6
Suhaib C <suhaib@uitoux.com>
2016-01-07 16:42:05 -0500

    Implemnt expand.

--- sites/larrylipshultz.com/themes/larrylipshultz/js/larrylipshultz_custom.js
+++ sites/larrylipshultz.com/themes/larrylipshultz/js/larrylipshultz_custom.js
@@ -2,7 +2,11 @@
   // Sample behavior
   Drupal.behaviors.larrylipshultz_theme_default_behavior = {
     attach: function(context, settings) {
-
+      $('.region-postscript-second a.expand', context).click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('.region-postscript-second .webform-component--message', context).slideToggle();
+      });
     }
   }
 })(jQuery);
