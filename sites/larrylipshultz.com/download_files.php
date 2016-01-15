ba8fb430951aeffe4e5d2d4f43571c0176c8c126
Suhaib C <suhaib@uitoux.com>
2016-01-15 13:05:54 -0500

    Update regex to avoid quote ambiguity.

--- sites/larrylipshultz.com/download_files.php
+++ sites/larrylipshultz.com/download_files.php
@@ -7,9 +7,11 @@ $data = db_select("field_data_body", 'b')
 
 while($node = $data->fetchObject()) {
 	$body = $node->body_value;
-	preg_match('/\<img(\s+[a-zA-Z\_\-]+\s*\=\s*[\'\"][^\'\"]*[\'\"])*(\s+src\s*\=\s*[\'\"]([^\'\"]*)[\'\"])\s*\/?\>/', $body, $tags);
-	if(!empty($tags[3])) {
+	preg_match_all('/\<img(\s+[a-zA-Z\_\-]+\s*\=\s*[\"][^\"]*[\"])*(\s+[a-zA-Z\_\-]+\s*\=\s*[\'][^\']*[\'])*(\s+src\s*\=\s*[\'\"]([^\'\"]*)[\'\"])(\s+[a-zA-Z\_\-]+\s*\=\s*[\"][^\"]*[\"])*(\s+[a-zA-Z\_\-]+\s*\=\s*[\'][^\']*[\'])*\s*\/?\>/', $body, $tags);
+
+	if(!empty($tags[4])) {
 		echo "Node: ".$node->entity_id."\n";
-		echo "Image: ".$tags[3]."\n\n";
+		echo "Images: \n";
+		print_r($tags[4]);
 	}
 }
