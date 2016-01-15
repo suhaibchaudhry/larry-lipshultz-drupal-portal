0113813ae98dc1d1c2bb91eeab802c65a6a2230c
Suhaib C <suhaib@uitoux.com>
2016-01-15 12:16:36 -0500

    Start script.

--- sites/larrylipshultz.com/download_files.php
+++ sites/larrylipshultz.com/download_files.php
@@ -1,2 +1,10 @@
 <?php
 echo "Downloading Images\n";
+$data = db_select("field_data_body", 'b')
+			->fields('b')
+			->condition('bundle', array('page', 'article', 'blog'), 'IN')
+			->execute();
+
+while($node = $data->fetchObject()) {
+	print_r($node);
+}
