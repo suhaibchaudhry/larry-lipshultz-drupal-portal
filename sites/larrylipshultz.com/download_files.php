fbcf6f98822d1cd8aa01841973bb28cde17da47b
Suhaib C <suhaib@uitoux.com>
2016-01-15 13:24:58 -0500

    Generate unique filename.

--- sites/larrylipshultz.com/download_files.php
+++ sites/larrylipshultz.com/download_files.php
@@ -1,4 +1,5 @@
 <?php
+$base = '/home/uitoux/larrylipshultz.com/httpdocs/sites/larrylipshultz.com/files/downloads';
 echo "Downloading Images\n";
 $data = db_select("field_data_body", 'b')
 			->fields('b')
@@ -10,8 +11,18 @@ while($node = $data->fetchObject()) {
 	preg_match_all('/\<img(\s+[a-zA-Z\_\-]+\s*\=\s*[\"][^\"]*[\"])*(\s+[a-zA-Z\_\-]+\s*\=\s*[\'][^\']*[\'])*(\s+src\s*\=\s*[\'\"]([^\'\"]*)[\'\"])(\s+[a-zA-Z\_\-]+\s*\=\s*[\"][^\"]*[\"])*(\s+[a-zA-Z\_\-]+\s*\=\s*[\'][^\']*[\'])*\s*\/?\>/', $body, $tags);
 
 	if(!empty($tags[4])) {
-		echo "Node: ".$node->entity_id."\n";
-		echo "Images: \n";
-		print_r($tags[4]);
+		//echo "Node: ".$node->entity_id."\n";
+		//echo "Images: \n";
+		foreach($tags[4] as $src) {
+			if(!empty($src)) {
+				echo downloadImage($src)."\n";
+			}
+		}
 	}
+}
+
+function downloadImage($src) {
+	preg_match('/[^\.]*\.([^\?]*)/', basename($src), $ext);
+	$file = tempnam('', 'image_').'.'.$ext[1];
+	return basename($file);
 }
