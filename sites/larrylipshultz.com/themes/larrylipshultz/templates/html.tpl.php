f9f97022448be4281e48c1c4261da31d27aee121
Suhaib C <suhaib@uitoux.com>
2016-09-02 16:35:16 -0400

    Add tracking code.

--- sites/larrylipshultz.com/themes/larrylipshultz/templates/html.tpl.php
+++ sites/larrylipshultz.com/themes/larrylipshultz/templates/html.tpl.php
@@ -9,6 +9,26 @@
   <script type="text/javascript">
   (function(a,e,c,f,g,h,b,d){var k={ak:"949014976",cl:"a8_oCKOe4GkQwKPDxAM"};a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)};a[g]||(a[g]=k.ak);b=e.createElement(h);b.async=1;b.src="//www.gstatic.com/wcm/loader.js";d=e.getElementsByTagName(h)[0];d.parentNode.insertBefore(b,d);a[f]=function(b,d,e){a[c](2,b,k,d,null,new Date,e)};a[f]()})(window,document,"_googWcmImpl","_googWcmGet","_googWcmAk","script");
   </script>
+  <?php if($_GET['q'] == "node/165/done") { ?>
+  <!-- Google Code for Completed Contact Form Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 949014976;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "IIcVCKeJumMQwKPDxAM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/949014976/?label=IIcVCKeJumMQwKPDxAM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>  
+  <?php } ?>
 </head>
 <body<?php print $attributes;?> onload="_googWcmGet('phnumber', '(713) 798-4001')">
   <div id="skip-link">
